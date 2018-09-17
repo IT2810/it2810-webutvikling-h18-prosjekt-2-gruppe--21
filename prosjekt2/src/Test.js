e178183019098ddb5bf287f3397ca29189949e16
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2018-09-17 15:11:19 +0200

    Create main layout

---
+++ prosjekt2/src/Test.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Test.css';
+
+class Test extends Component {
+  render() {
+    return (
+        <div className="grid-container">
+            <div className="visual-display">
+                <div className="title">
+
+                </div>
+                <div className="tab-container">
+                  <div className="tab" id="0"></div>
+                  <div className="tab" id="1"></div>
+                  <div className="tab" id="2"></div>
+                  <div className="tab" id="3"></div>
+                </div>
+                <div className="image">
+
+                </div>
+                <div className="poem">
+
+                </div>
+                
+            </div>
+            <div className="categories">
+            <h1>TEST</h1>
+            </div>
+      </div>
+    );
+  }
+}
+
+export default Test;
