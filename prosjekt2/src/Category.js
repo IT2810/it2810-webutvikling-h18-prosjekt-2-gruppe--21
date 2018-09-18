5e62d37a1f165e40a62bee042f3450eb350b1179
Thomas <skarshaug.thomas@gmail.com>
2018-09-18 23:14:31 +0200

    Create main layout - added names to the categories and gave them padding

--- prosjekt2/src/Category.js
+++ prosjekt2/src/Category.js
@@ -7,22 +7,54 @@ export class Category extends Component {
   render() {
     return (
       <div className="radioButtons">
-         <label className="radioButton">small</label>
-         <input type = "radio"
-                 name = "radSize"
-                 id = "sizeSmall"
-                 value = "small"
-                 checked = "checked" />
-          <label className="radioButton">medium</label>
-          <input type = "radio"
-                 name = "radSize"
-                 id = "sizeMed"
-                 value = "medium" />
-          <label className="radioButton">large</label>
-          <input type = "radio"
-                 name = "radSize"
-                 id = "sizeLarge"
-                 value = "large" /> 
+        <div className="radioGroup" id="1">
+            <h2>Image</h2>
+            <label className="radioButton">Skog og bekkefar</label>
+            <input type = "radio"
+                name = "radSize"
+                value = "small"
+                checked = "checked" />
+             <label className="radioButton">Fugler og hav</label>
+             <input type = "radio"
+                name = "radSize"
+                value = "medium" />
+             <label className="radioButton">Fjell og vidde</label>
+             <input type = "radio"
+                name = "radSize"
+                value = "large" /> 
+        </div>
+        <div className="radioGroup" id="2">
+            <h2>Sound</h2>
+            <label className="radioButton">Skog og bekkefar</label>
+            <input type = "radio"
+                name = "radSize"
+                value = "small"
+                checked = "checked" />
+            <label className="radioButton">Fugler og hav</label>
+            <input type = "radio"
+                name = "radSize"
+                value = "medium" />
+              <label className="radioButton">Fjell og vidde</label>
+              <input type = "radio"
+                name = "radSize"
+                value = "large" /> 
+        </div>
+        <div className="radioGroup" id="3">
+            <h2>Text</h2>
+             <label className="radioButton">Skog og bekkefar</label>
+             <input type = "radio"
+                name = "radSize"
+                value = "small"
+                checked = "checked" />
+             <label className="radioButton">Fugler og hav</label>
+             <input type = "radio"
+                name = "radSize"
+                value = "medium" />
+              <label className="radioButton">Fjell og vidde</label>
+              <input type = "radio"
+                name = "radSize"
+                value = "large" /> 
+        </div>
       </div>
     );
   }
