eea32de817a97e437b51c25323d13ecc26b88155
Simen Kjernlie <simenkjernlie@Simens-MacBook-Pro.local>
2018-09-19 08:20:29 +0200

    added responsive tabs

--- prosjekt2/src/Test.js
+++ prosjekt2/src/Test.js
@@ -1,33 +1,26 @@
-
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './Test.css';
-import {Category} from './Category.js';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./Test.css";
+import { Category } from "./Category.js";
 
 class Test extends Component {
   render() {
     return (
-        <div className="grid-container">
-            <div className="visual-display">
-                <div className="title">
-                </div>
-                <div className="tab-container">
-                  <div className="tab" id="0"></div>
-                  <div className="tab" id="1"></div>
-                  <div className="tab" id="2"></div>
-                  <div className="tab" id="3"></div>
-                </div>
-                <div className="image">
-
-                </div>
-                <div className="poem">
-
-                </div>
-                
-            </div>
-            <div className="categories">
-                <Category />
-            </div>
+      <div className="grid-container">
+        <div className="visual-display">
+          <div className="title" />
+          <div className="tab-container">
+            <div className="tab" id="0" />
+            <div className="tab" id="1" />
+            <div className="tab" id="2" />
+            <div className="tab" id="3" />
+          </div>
+          <div className="image" />
+          <div className="poem" />
+        </div>
+        <div className="categories">
+          <Category />
+        </div>
       </div>
     );
   }
