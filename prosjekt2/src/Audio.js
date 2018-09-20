957b52c4a8234ac4b28c8ef0ddc7c5b276481ad0
Simen Kjernlie <simen.kjernlie@gmail.com>
2018-09-20 15:28:48 +0200

    added looping of audio

--- prosjekt2/src/Audio.js
+++ prosjekt2/src/Audio.js
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 export class Audio extends Component {
   state = { resourcepath: null };
   render() {
-    return <audio src={this.state.resourcepath} autoPlay={true} />;
+    return <audio src={this.state.resourcepath} autoPlay={true} loop={true} />;
   }
 }
